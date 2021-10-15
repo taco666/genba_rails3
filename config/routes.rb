19411bbbaf907595bf99315907e8f8dcbec421fe
taco666 <statw56@gmail.com>
2021-10-15 15:51:28 +0900

    Chapter6-4

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,4 @@ Rails.application.routes.draw do
   end
   root to: 'tasks#index'
   resources :tasks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
